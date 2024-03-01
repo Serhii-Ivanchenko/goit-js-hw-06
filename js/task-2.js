b365910c57ae33aa1e7f6c1d9dd401733a782231
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-03-01 21:12:59 +0200

    task-2 fixed 2

--- js/task-2.js
+++ js/task-2.js
@@ -13,7 +13,9 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    if (this.#items.includes(itemToRemove)) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
   }
 }
 
@@ -23,3 +25,5 @@ storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// Неправильна реалізація removeItem: метод removeItem використовує splice безпосередньо з результатом indexOf. Якщо itemToRemove не знайдено, indexOf повертає -1, що змушує splice видаляти останній елемент з масиву, що є неправильною поведінкою. Для виправлення цього, вам слід перевірити, чи елемент існує, перед використанням splice.
